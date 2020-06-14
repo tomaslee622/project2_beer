bc2038d114f0f7f1068c237957b26d16f6bf0f19
Ch-oi <gmu1233@gmail.com>
2020-06-14 22:40:28 +0800

    Database design modified: New bills table addded

--- migrations/20200609175943_users.js
+++ migrations/20200609175943_users.js
@@ -3,6 +3,10 @@ exports.up = function(knex) {
         table.increments();
         table.string('email').unique().notNullable();
         table.string('password');
+        table.string('first_name');
+        table.string('last_name');
+        table.string('phone');
+        table.date('birthday');
         table.string('propic_path');
         table.string('google_id');
         table.string('facebook_id');
--- migrations/20200611165544_purchase.js
+++ migrations/20200611165544_purchase.js
@@ -5,6 +5,8 @@ exports.up = function(knex) {
         table.foreign('user_id').references('users.id');
         table.integer('beer_id').unsigned().notNullable();
         table.foreign('beer_id').references('beers.id');
+        table.integer('bills').unsigned().defaultTo(0);
+        table.foreign('bills').references('bills.id');
         table.integer('quantity').unsigned().notNullable();
         table.integer('price').unsigned().notNullable();
         table.boolean('bought').notNullable();
